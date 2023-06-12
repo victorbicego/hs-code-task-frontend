c83acf738d943eb85dcc7ac1a05c2bfc44c3db6c
victor.bicego <victor.bicego@gmail.com>
2023-06-12 19:46:05 +0200

    create event component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
