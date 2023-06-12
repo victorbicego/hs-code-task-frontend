fb7b1e03749a74cdc5fab3b71e04634601c7b2b9
victor.bicego <victor.bicego@gmail.com>
2023-06-12 21:57:02 +0200

    add html and css to delete event modal

--- src/app/delete-event-modal/delete-event-modal.component.ts
+++ src/app/delete-event-modal/delete-event-modal.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-delete-event-modal',
   templateUrl: './delete-event-modal.component.html',
-  styleUrls: ['./delete-event-modal.component.css']
+  styleUrls: ['./delete-event-modal.component.css'],
 })
 export class DeleteEventModalComponent {
+  @Output() confirmDelete: EventEmitter<void> = new EventEmitter<void>();
+  @Output() closeModalEvent: EventEmitter<void> = new EventEmitter<void>();
 
+  closeModal(): void {
+    this.closeModalEvent.emit();
+  }
+
+  deleteEvent(): void {
+    this.confirmDelete.emit();
+  }
 }
