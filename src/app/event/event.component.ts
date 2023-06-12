48e2c97d1d62706351878c96bd9b341b3d1c2c87
victor.bicego <victor.bicego@gmail.com>
2023-06-12 21:45:18 +0200

    add html and css

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -12,4 +12,9 @@ export class EventComponent {
   showDescription: boolean = false;
   showPrice: boolean = false;
   showCapacity: boolean = false;
+  showDeleteEventModal: boolean = false;
+
+  handleCloseDeleteEventModal(): void {
+    this.showDeleteEventModal = false;
+  }
 }
