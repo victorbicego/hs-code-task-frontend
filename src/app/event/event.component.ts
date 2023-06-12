f9f2e0e1e46a5c09eccf40fceb798eb3ed5976ae
victor.bicego <victor.bicego@gmail.com>
2023-06-12 20:11:19 +0200

    add input to event component

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Event } from '../interfaces/event';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+  styleUrls: ['./event.component.css'],
 })
 export class EventComponent {
-
+  @Input() event?: Event;
 }
